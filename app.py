3b541207b5b89cf88385103048092cbe280f6554
anarshah <javacodesmaster@gmail.com>
2023-04-11 06:21:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -35,14 +35,23 @@ def get_input():
 
 # Define a function to preprocess the inputs
 def preprocess_inputs(inputs):
-    # Load the car data from the CSV file
-    df_car = pd.read_csv('olx_car_data_csv.csv', encoding='ISO-8859-1')
+    # Load the dataset
+    data = pd.read_csv('olx_car_data_csv.csv', encoding='ISO-8859-1')
+
+    # Preprocess the data
+    # Drop unnecessary columns
+    data = data.drop(['Registered City', 'Transaction Type'], axis=1)
 
     # Convert categorical variables into numeric form
-    df_car['Brand'] = pd.factorize(df_car['Brand'])[0]
-    df_car['Condition'] = pd.factorize(df_car['Condition'])[0]
-    df_car['Fuel'] = pd.factorize(df_car['Fuel'])[0]
-    df_car['Model'] = pd.factorize(df_car['Model'])[0]
+    data['Brand'] = pd.factorize(data['Brand'])[0]
+    data['Condition'] = pd.factorize(data['Condition'])[0]
+    data['Fuel'] = pd.factorize(data['Fuel'])[0]
+    data['Model'] = pd.factorize(data['Model'])[0]
+
+    # Impute missing values in the input data
+    imputer = SimpleImputer(strategy='median')
+    X = imputer.fit_transform(data.drop('Price', axis=1))
+    y = data['Price']
     
     # Convert user inputs into numeric form
     inputs['Brand'] = df_car.loc[df_car['Brand'] == inputs['Brand'], 'Brand'].index[0]
